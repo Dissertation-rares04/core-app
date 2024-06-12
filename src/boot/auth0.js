d3a295efd524adb51e7d6cd92ca69a45dda5bb93
rastilean <raresastilean@yahoo.com>
2024-06-12 22:34:40 +0300

    Add Auth0 env variables

--- src/boot/auth0.js
+++ src/boot/auth0.js
@@ -1,25 +1,25 @@
-import { boot } from 'quasar/wrappers'
-import { createAuth0 } from '@auth0/auth0-vue';
+import { boot } from "quasar/wrappers";
+import { createAuth0 } from "@auth0/auth0-vue";
 
 const auth0 = createAuth0({
-  domain: 'dev-re5ucc1nn12lb5qo.us.auth0.com',
-  clientId: '7PsLF1tiowoy50Ru4EW4EiWwk3uG6ylE',
-  cacheLocation: 'localstorage',
+  domain: process.env.AUTH0_DOMAIN,
+  clientId: process.env.AUTH0_CLIENTID,
+  cacheLocation: "localstorage",
   useRefreshTokens: true,
   authorizationParams: {
-    audience: 'https://hello-world.example.com',
-    redirect_uri: 'http://localhost:4000/callback'
-  }
-})
+    audience: process.env.AUTH0_AUDIENCE,
+    redirect_uri: process.env.AUTH0_REDIRECT_URI,
+  },
+});
 
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 export default boot(({ app, router }) => {
-  app.config.globalProperties.$router = router
+  app.config.globalProperties.$router = router;
 
-  app.use(auth0)
+  app.use(auth0);
 
-  app.config.globalProperties.$auth0 = auth0
-})
+  app.config.globalProperties.$auth0 = auth0;
+});
 
-export { auth0 }
+export { auth0 };
--- substitute_environment_variables.sh
+++ substitute_environment_variables.sh
@@ -6,6 +6,10 @@ ROOT_DIR=/app
 for file in $ROOT_DIR/js/*.js* $ROOT_DIR/index.html $ROOT_DIR/precache-manifest*.js;
 do
   sed -i 's|API_URL_PLACEHOLDER|'${API_URL}'|g' $file
+  sed -i 's|AUTH0_DOMAIN_PLACEHOLDER|'${AUTH0_DOMAIN}'|g' $file
+  sed -i 's|AUTH0_CLIENTID_PLACEHOLDER|'${AUTH0_CLIENTID}'|g' $file
+  sed -i 's|AUTH0_AUDIENCE_PLACEHOLDER|'${AUTH0_AUDIENCE}'|g' $file
+  sed -i 's|AUTH0_REDIRECT_URI_PLACEHOLDER|'${AUTH0_REDIRECT_URI}'|g' $file
   # Your other variables here...
 done
 # Let container execution proceed
